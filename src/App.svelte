7382d0a56f48255ada0476f016bccdfb6fa73d17
Francisco Ezequiel Baños Ramirez <chekelosos@gmail.com>
2023-12-17 22:43:33 -0600

    Added main structure and styling for the landing page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
 </script>
 
-<main>
-  <p class=" text-red-500">sddsaadsahsjhsjkhsh</p>
+<main class="flex items-center justify-center absolute w-[100vw] h-[100vh] bg-[#1e555c]">
+  <div class=" w-2/4 h-4/6 flex flex-col items-center justify-start text-white gap-10">
+    <h1 class=" text-5xl font-bold">25 + 5 Clock</h1>
+    <div class="flex justify-between w-[50%]">
+      <p>break length</p>
+      <p>session length</p>
+    </div>
+  </div>
 </main>
